309d7df2c6704c4b22b0d426f2e4cc18c774fb59
Ja5in99 <jasin1999@hotmail.se>
2022-09-10 17:41:37 +0200

    module-2

---
+++ client/src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Courses from "../components/Courses";
+
+
+const Homepage = () => {
+    return <div>
+      <Courses />
+    </div>;
+  };
+  
+  export default Homepage;
