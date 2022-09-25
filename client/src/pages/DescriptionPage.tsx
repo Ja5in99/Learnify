167317d5a6c0929b239878a971e5e3e411223d23
Ja5in99 <jasin1999@hotmail.se>
2022-09-25 13:16:21 +0200

    added remove from basket

--- client/src/pages/DescriptionPage.tsx
+++ client/src/pages/DescriptionPage.tsx
@@ -2,22 +2,17 @@ import React, { useEffect, useState } from "react";
 import { Course, Learning, Requirement } from "../models/course";
 import { useParams } from "react-router";
 import agent from "../actions/agent";
-import { useStoreContext } from "../context/StoreContext";
+import { useAppDispatch, useAppSelector } from '../redux/store/configureStore';
 import { Link } from "react-router-dom";
+import { addBasketItemAsync} from "../redux/slice/basketSlice";
 
 const DescriptionPage = () => {
   const [course, setCourse] = useState<Course>();
   const { id } = useParams<{ id: string }>();
 
-  const { basket, setBasket } = useStoreContext();
+  const { basket } = useAppSelector((state) => state.basket);
+  const dispatch = useAppDispatch();
 
-  const addToCart = (courseId: string) => {
-    agent.Baskets.addItem(courseId)
-      .then((response) => setBasket(response))
-      .catch((error) => {
-        console.log(error);
-      });
-  };
 
   useEffect(() => {
     agent.Courses.getById(id).then((response) => {
@@ -160,7 +155,7 @@ const DescriptionPage = () => {
               </Link>
             ) : (
               <div
-                onClick={() => addToCart(course!.id)}
+                onClick={() => dispatch(addBasketItemAsync({courseId:course!.id}))}
                 className="description-page__sidebar__box__button--cart"
               >
                 Add to cart
